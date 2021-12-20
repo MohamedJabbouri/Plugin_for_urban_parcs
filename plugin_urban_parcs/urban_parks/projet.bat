52814bbdb21ff35cf4c283fab29729f506ebe887
MohamedJabbouri <95582477+MohamedJabbouri@users.noreply.github.com>
2021-12-20 20:48:23 +0100

    Add files via upload

---
+++ plugin_urban_parcs/urban_parks/projet.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call "C:\Program Files\QGIS 3.16.12\bin\o4w_env.bat"	
+call "C:\Program Files\QGIS 3.16.12\bin\qt5_env.bat"
+call "C:\Program Files\QGIS 3.16.12\bin\py3_env.bat"
+
+
+@echo on
+pyrcc5 -o resources.py tesources.qrt
